5ce2bfb0b436f930696fec522fab6a494b5a3de4
ArturoAlvarado <arturo.alvarado@wizeline.com>
2020-10-05 19:34:15 -0500

    login

--- src/pages/Login/Login.page.jsx
+++ src/pages/Login/Login.page.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router';
 import { Redirect } from 'react-router-dom';
 import { useAuth } from '../../providers/Auth';
@@ -6,12 +6,15 @@ import './Login.styles.css';
 
 function LoginPage() {
   const { authenticated, login } = useAuth();
+  const [ credentials, setCredentials ] = useState({ user: '', password: '' })
   const history = useHistory();
 
   function authenticate(event) {
-    event.preventDefault();
-    login();
-    history.push('/');
+    if (credentials.user === 'user' && credentials.password === 'password') {
+      event.preventDefault();
+      login();
+      history.push('/');
+    }
   }
 
   return (
@@ -19,29 +22,29 @@ function LoginPage() {
       {authenticated ? (
         <Redirect to="/" />
       ) : (
-        <div className="login_container">
-          <section className="login">
-            <h1>Welcome back!</h1>
-            <form onSubmit={authenticate} className="login-form">
-              <div className="form-group">
-                <label htmlFor="username">
-                  <strong>username </strong>
-                  <input required type="text" id="username" />
-                </label>
-              </div>
-              <div className="form-group">
-                <label htmlFor="password">
-                  <strong>password </strong>
-                  <input required type="password" id="password" />
-                </label>
-              </div>
-              <button id="login" type="submit">
-                login
+          <div className="login_container">
+            <section className="login">
+              <h1>Welcome back!</h1>
+              <form onSubmit={authenticate} className="login-form">
+                <div className="form-group">
+                  <label htmlFor="username">
+                    <strong>username </strong>
+                    <input required type="text" id="username" onChange={(e) => setCredentials({ ...credentials, user: e.target.value })} />
+                  </label>
+                </div>
+                <div className="form-group">
+                  <label htmlFor="password">
+                    <strong>password </strong>
+                    <input required type="password" id="password" onChange={(e) => setCredentials({ ...credentials, password: e.target.value })} />
+                  </label>
+                </div>
+                <button id="login" type="submit">
+                  login
               </button>
-            </form>
-          </section>
-        </div>
-      )}
+              </form>
+            </section>
+          </div>
+        )}
     </>
   );
 }
